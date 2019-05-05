6e0ac84541d61da88bdce5773a4fea2159a377e5
floflimctb <floflo.brotons@gmail.com>
2019-05-05 16:08:59 +0200

    First version: hero

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html">
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Way of tea - Accueil</title>
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="normalize.css" />
+    </head>
+  
+    <body>
+        <!--  Start Hero Section  -->
+        <section class="hero">
+            <header>
+                <div class="row">
+                    <nav class="top-bar" data-topbar role="navigation">
+            
+                        <!--    Start Logo    -->
+                        <ul class="title-area">
+                            <li class="name"><a href="#" class="logo"><h1>Way of tea</h1></a></li>
+                            <li><span class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></span></li>
+                        </ul>
+                        <!--    End Logo    -->
+
+                        <!--    Start Navigation Menu    -->
+                        <section class="top-bar-section" id="mean_nav">
+                          <ul class="right">
+                            <li><a href="couleurs.php">Les différentes couleurs de thé</a></li>
+                            <li><a href="propos.php">A propos</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                          </ul>
+                        </section>
+                        <!--    End Navigation Menu    -->
+
+                    </nav>
+                </div>
+            </header>
+
+            <!--    Start Hero Caption    -->
+            <section class="caption">
+                <div class="row">
+                    <h1 class="mean_cap">Responsive Template</h1>
+                    <h2 class="sub_cap">Less Is More.</h2>
+                    <a href="#" class="btn_details"><span>More Details</span> <img src="img/btn_arrow.png" alt="" src="" class="arrow"></a>
+                </div>
+            </section>
+            <!--    End Hero Caption    -->
+
+        </section>
+        
+        <!--
+  <div id="conteneur">    
+    <h1 id="header"><a href="#" title="Colored Design - Accueil"><span>Victor</span></a></h1>
+
+    <nav>
+      <ul id="menu">
+        <li><a href="#">Etape n°1</a></li>
+        <li><a href="#">A propos</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    
+    <div id="contenu">
+      <h2>Un titre</h2>
+      <p>Un paragraphe</p>
+    </div>
+    
+    <p id="footer">Réalisation des codes HTML/CSS, du tutoriel et du design par ElMoustiko</p>
+  </div>-->
+    </body>
+</html>
