a9b3e5ba68fa22c59ed4dce585e5cb84c4a24cc4
floflimctb <floflo.brotons@gmail.com>
2019-05-05 18:04:00 +0200

    Changement de style

--- index.php
+++ index.php
@@ -4,67 +4,67 @@
         <meta charset="utf-8" />
         <title>Way of tea - Accueil</title>
         <link rel="stylesheet" href="style.css" />
-        <link rel="stylesheet" href="normalize.css" />
     </head>
   
     <body>
-        <!--  Start Hero Section  -->
-        <section class="hero">
+        <div id="bloc_page">
             <header>
-                <div class="row">
-                    <nav class="top-bar" data-topbar role="navigation">
-            
-                        <!--    Start Logo    -->
-                        <ul class="title-area">
-                            <li class="name"><a href="#" class="logo"><h1>Way of tea</h1></a></li>
-                            <li><span class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></span></li>
+                <p><a href="index.php"><img src="images/logo-transparent.png" alt="Logo" width="223px" height="114px" /></a></p>
+
+                <div id="menu">
+                    <nav>
+                        <ul>
+                            <li><a href="couleurs.php"><img src="images/feuille-transparent.png" alt="feuille" width="35px" height="40px" /> Les différentes couleurs de thé</a></li>
+                            <li><a href="propos.php"><img src="images/feuille-transparent.png" alt="feuille" width="35px" height="40px" />A propos</a></li>
+                            <li><a href="contact.php"><img src="images/feuille-transparent.png" alt="feuille" width="35px" height="40px" />Contact</a></li>
                         </ul>
-                        <!--    End Logo    -->
-
-                        <!--    Start Navigation Menu    -->
-                        <section class="top-bar-section" id="mean_nav">
-                          <ul class="right">
-                            <li><a href="couleurs.php">Les différentes couleurs de thé</a></li>
-                            <li><a href="propos.php">A propos</a></li>
-                            <li><a href="contact.php">Contact</a></li>
-                          </ul>
-                        </section>
-                        <!--    End Navigation Menu    -->
-
                     </nav>
                 </div>
             </header>
+            
+            <section>
+                <article>
+                    <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper massa sit amet placerat euismod. Proin finibus ligula sed condimentum condimentum. Aliquam lorem nisi, auctor non nunc laoreet, congue consequat lectus. Maecenas id nunc non massa lobortis facilisis eget tincidunt tellus. Vestibulum fringilla, odio ut tristique fermentum, mauris nisl tincidunt tortor, sit amet blandit arcu mi et orci. Cras vehicula diam vel magna pretium, id efficitur turpis volutpat. Aenean purus mauris, porttitor vel ultrices sit amet, faucibus id lacus. Sed suscipit tincidunt euismod. Nunc dignissim lectus at suscipit mattis. Pellentesque hendrerit, sapien sed mollis pretium, felis ligula efficitur ex, vel dignissim lorem magna tempus arcu. In maximus non dolor sit amet molestie. Praesent nunc eros, viverra ut enim nec, auctor vehicula velit. Nam accumsan ac nisl et suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec sit amet ex vel justo convallis venenatis. Integer venenatis neque diam, tempor fermentum justo sodales id.
+                        </p>
+                        
+                        <p>
+                            <b>Quisque vitae mollis nisl.</b> Aliquam fermentum porttitor vestibulum. Vivamus ut velit arcu. Cras leo ante, vulputate nec felis non, euismod commodo risus. Vivamus bibendum orci eget eros egestas, dignissim sollicitudin est efficitur. Vivamus sed mauris dapibus dolor lacinia vulputate. Sed suscipit vitae neque ac cursus. Vivamus sed laoreet velit.
+                        </p>
+                        
+                        <p>
+                            <b>Curabitur rhoncus ante sed lorem porttitor posuere.</b> Nullam tincidunt, est ut rutrum posuere, sapien turpis aliquet lacus, a tristique turpis libero eget arcu. Phasellus accumsan commodo felis, dictum sollicitudin mauris ullamcorper et. Aliquam non justo at mauris consectetur rutrum. Proin viverra posuere mauris a lacinia. Sed eget metus at libero cursus rhoncus sit amet vitae erat. Nam fringilla risus sed felis eleifend, at sollicitudin tellus ultricies. Fusce interdum nisi ac dolor finibus imperdiet. Proin commodo ornare ipsum, a pellentesque enim ullamcorper quis. Ut venenatis justo vel molestie gravida. Maecenas consequat lorem nec quam mattis eleifend. Nulla non bibendum ipsum. Etiam ornare sem purus, at pellentesque sapien scelerisque nec. Fusce dictum ultricies ligula, nec cursus felis porttitor sed. Aliquam a justo non libero egestas sagittis vel ac metus.
+                        </p>
+                        
+                        <p>
+                            Praesent mattis velit purus, sit amet dignissim ex sagittis non. Fusce hendrerit sem enim, sed euismod tellus sollicitudin lacinia. Vestibulum non convallis sem. Quisque condimentum vulputate ante, sit amet lacinia diam vulputate quis. Aliquam accumsan diam pharetra dapibus lacinia. Duis urna ligula, rhoncus a pharetra in, efficitur sed ante. Fusce eu tincidunt nibh. Praesent varius sit amet nisi eget accumsan. Vestibulum a enim mauris. Vivamus hendrerit molestie feugiat. Vivamus quis elementum neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse luctus consequat lectus, eget tempor sem blandit sed.
+                        </p>
+                        
+                        <p>
+                            Mauris egestas tempor volutpat. Nullam congue justo a ipsum rutrum posuere. Phasellus rutrum suscipit tincidunt. Vivamus semper augue in leo blandit dapibus. Ut at metus mattis, pharetra nisl non, congue massa. Vestibulum elit orci, pretium quis eleifend vitae, molestie sit amet nulla. Integer gravida felis sit amet sem aliquet, sit amet ullamcorper est pretium. Ut accumsan facilisis fermentum. Nulla id metus placerat, lacinia nibh id, dapibus lorem. Pellentesque lectus mi, viverra vel eros a, blandit accumsan lectus. Cras facilisis mauris eu enim pharetra, ac semper nunc mollis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt enim quis pretium cursus. Donec ornare odio non neque accumsan dictum.
+                        </p>
 
-            <!--    Start Hero Caption    -->
-            <section class="caption">
-                <div class="row">
-                    <h1 class="mean_cap">Responsive Template</h1>
-                    <h2 class="sub_cap">Less Is More.</h2>
-                    <a href="#" class="btn_details"><span>More Details</span> <img src="img/btn_arrow.png" alt="" src="" class="arrow"></a>
-                </div>
+                        <p>
+                            In tempor mi id eros dapibus, sed venenatis odio volutpat. Praesent at viverra urna. Curabitur sed risus facilisis, rutrum est ac, interdum nibh. Duis posuere tincidunt nulla facilisis cursus. Donec lacinia, arcu vel facilisis pellentesque, libero mauris cursus mi, vitae interdum leo dui sit amet est. Aliquam id dignissim orci. Nulla facilisi. Proin egestas aliquam libero, sit amet faucibus diam ultrices in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc congue neque et eros bibendum, sit amet rutrum turpis faucibus. Suspendisse tempor quam metus, id ultricies magna hendrerit ac. Nulla posuere rutrum egestas.
+                        </p>
+
+                        <p>
+                            Fusce congue, velit varius congue fringilla, nunc felis auctor risus, et commodo leo sem ut magna. Duis euismod porta nunc, sagittis scelerisque turpis iaculis commodo. Fusce pharetra vitae libero tincidunt fermentum. Praesent sit amet eros ultricies, vestibulum massa at, ultrices dolor. Sed porttitor turpis in risus volutpat, eu pellentesque nibh sagittis. Sed mollis, erat id venenatis finibus, quam ante semper tortor, at maximus ipsum leo nec massa. Proin pretium dignissim blandit. Duis eleifend tempus congue. Cras tellus justo, eleifend non leo vel, iaculis cursus elit. Integer non neque nec enim ornare congue at eu nisl. Fusce scelerisque lobortis magna, eu feugiat felis rhoncus eu. Nam eu metus et augue fermentum finibus. Integer felis libero, venenatis vitae commodo vel, vestibulum in ipsum. Aenean ut porta ipsum. Aliquam fringilla viverra lectus ac volutpat.
+                        </p>
+
+                        <p>
+                            Phasellus consequat, sem ac pharetra tincidunt, nunc ligula mollis odio, quis lobortis lorem ante sit amet diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris massa ipsum, tristique eget sapien at, tempus fermentum sapien. Mauris consequat vitae nisi in aliquet. Morbi malesuada dui sed placerat imperdiet. Vivamus a lobortis nunc, eget eleifend justo. Phasellus in quam mauris. Praesent sed commodo nibh, sit amet rutrum massa. Donec gravida elementum nibh ac sollicitudin. Maecenas in iaculis felis, et tempor justo. Fusce blandit diam elit, convallis scelerisque turpis viverra sed.
+                        </p>
+
+                        <p>
+                            Nulla pharetra congue venenatis. Nunc fermentum quis est eget aliquet. Suspendisse id quam a mauris tincidunt fermentum sed vitae felis. Pellentesque eu turpis dui. Aenean eleifend turpis convallis luctus interdum. Sed non elit dictum, iaculis sapien eget, finibus velit. Aliquam vitae vestibulum ex, nec laoreet dolor. Curabitur et tempor mi. Fusce venenatis tortor at ex vulputate maximus. Vestibulum malesuada sollicitudin aliquam.
+                        </p>
+
+                        <p>
+                            Donec vel mi non urna vehicula auctor sit amet eu orci. Aliquam at sodales dui, id sodales libero. Nullam egestas scelerisque nisi. Pellentesque viverra mattis lacus, in rutrum ex dapibus a. In eget diam libero. Sed diam turpis, varius sed ligula sit amet, interdum aliquet nulla. Etiam vel dignissim ligula. Aliquam egestas vulputate dui, et iaculis ligula elementum vitae. Nunc maximus nisl nisl, a eleifend tellus rutrum eget. Duis odio felis, semper eu ligula ut, mattis mattis metus. Phasellus accumsan, eros vitae laoreet consequat, nisi nibh dapibus tellus, ac ornare lacus nulla vitae dui. Nunc maximus auctor libero at sagittis. Aenean bibendum, lorem rhoncus placerat hendrerit, magna dolor auctor nulla, non dignissim leo purus non sapien. Fusce aliquam, orci vitae congue molestie, nisi nunc vestibulum arcu, ut venenatis urna dui ac nisi.
+                        </p>
+                </article>
             </section>
-            <!--    End Hero Caption    -->
-
-        </section>
-        
-        <!--
-  <div id="conteneur">    
-    <h1 id="header"><a href="#" title="Colored Design - Accueil"><span>Victor</span></a></h1>
-
-    <nav>
-      <ul id="menu">
-        <li><a href="#">Etape n°1</a></li>
-        <li><a href="#">A propos</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
-    
-    <div id="contenu">
-      <h2>Un titre</h2>
-      <p>Un paragraphe</p>
-    </div>
-    
-    <p id="footer">Réalisation des codes HTML/CSS, du tutoriel et du design par ElMoustiko</p>
-  </div>-->
+        </div>
     </body>
 </html>
